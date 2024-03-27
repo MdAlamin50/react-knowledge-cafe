71c1e9410258b8d74188d09415d37b444584197e
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-27 10:22:23 +0600

    added the timer setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,27 @@ import Header from './components/Hader/Header'
 
 function App() {
   const [bookmarks , setBookmarks] = useState([]);
-// new blog add
+   
+   const [readingTime,setRedingTime]=useState(0);
+
   const handleAddToBookmark=blog=>{
    const newBookmarks=[...bookmarks,blog];
    setBookmarks(newBookmarks)
   
+  }
+   
+  const handleMarkAsRead=time=>{
+    const newReadingTime = readingTime + time;
+    setRedingTime(newReadingTime);
+ 
   }
 
   return (
     <>
        <Header></Header>
       <div className='md:flex max-w-7xl mx-auto'>
-      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
-      <Bookmarks bookmarks={bookmarks}></Bookmarks>
+      <Blogs handleAddToBookmark={handleAddToBookmark}handleMarkAsRead={handleMarkAsRead}></Blogs>
+      <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
       </div>
        
     </>
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,17 +1,23 @@
-import PropTypes from 'prop-types'
-import Bookmark from '../Bookmark/Bookmark';
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 
-const Bookmarks = ({bookmarks}) => {
-    return (
-        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4 rounded-lg">
-            <h2 className="text-3xl text-center font-bold">Bookmarked Blogs:{bookmarks.length}</h2>
-            {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
-            }
+const Bookmarks = ({ bookmarks, readingTime }) => {
+  return (
+    <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4 rounded-lg">
+        <div className="bg-[#a99cf4] rounded-lg p-2">
+            <h3 className="text-4xl text-center mb-4 font-medium text-[#5537ff]">Reading Time:{readingTime}</h3>
         </div>
-    );
+      <h2 className="text-3xl text-center font-bold">
+        Bookmarked Blogs:{bookmarks.length}
+      </h2>
+      {bookmarks.map((bookmark) => (
+        <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
+      ))}
+    </div>
+  );
 };
-Bookmarks.propTypes={
-   bookmarks:PropTypes.array
-}
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array,
+  readingTime:PropTypes.number
+};
 export default Bookmarks;
